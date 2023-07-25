232adee1c9ed5e3a2c685c66f12721be0197a79f
HernandezKevin95 <62568586+HernandezKevin95@users.noreply.github.com>
2023-07-24 18:10:18 -0700

    started base site on app.js linking contact and about me, applied headers and footers with some styling

--- app.js
+++ app.js
@@ -13,6 +13,24 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("home.ejs", {
+    startingContent: homeStartingContent
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs", {
+    aboutContent: aboutContent
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs", {
+    contactContent: contactContent
+  });
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
